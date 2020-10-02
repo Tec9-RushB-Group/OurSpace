2c37bc28a5bede204b6698366803b6a86af0fda0
Austin Wright <austinwright000@gmail.com>
2020-10-02 14:25:07 -0400

    Changed a few files. + login screen now goes to MainScreenActivity + Started drawing the main screen and round buttons. Little harder than I orignally thought but thats okay. - The signup screen still needs to point to MainScreenActivity, I wasn't sure if there was anything else left to do on SignUp. TODO - Add checker for Null type value in MainScreenActivity

--- app/src/main/java/com/tec9rushbgroup/ourspace/MainScreenActivity.java
+++ app/src/main/java/com/tec9rushbgroup/ourspace/MainScreenActivity.java
@@ -3,12 +3,25 @@ package com.tec9rushbgroup.ourspace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainScreenActivity extends AppCompatActivity {
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String name = user.getDisplayName();
+
+    String[] firstName = name.split(" ");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        ((TextView)findViewById(R.id.hello)).setText(getString(R.string.welcome_message,firstName));
     }
+
+
 }
