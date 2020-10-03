f4c2d9d1b3a98f3ec61e8ad605ee522d4a6c0771
ZBMS01 <zbrs01@gmail.com>
2020-10-03 03:49:12 -0400

    small change

---
+++ app/src/main/java/com/tec9rushbgroup/ourspace/FindSpace.java
@@ -0,0 +1,16 @@
+package com.tec9rushbgroup.ourspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FindSpace extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_space);
+    }
+}
