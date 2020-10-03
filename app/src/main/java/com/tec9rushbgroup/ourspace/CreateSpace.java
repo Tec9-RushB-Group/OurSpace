b62f9b28d1ff18e6ebbc9de028d74e3b198afc01
ZBMS01 <zbrs01@gmail.com>
2020-10-03 05:49:06 -0400

    get window

--- app/src/main/java/com/tec9rushbgroup/ourspace/CreateSpace.java
+++ app/src/main/java/com/tec9rushbgroup/ourspace/CreateSpace.java
@@ -3,11 +3,13 @@ package com.tec9rushbgroup.ourspace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class CreateSpace extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_space);
 
