ad74182f9d3b1b297e824d42d51e85bef7bee292
ZBMS01 <zbrs01@gmail.com>
2020-10-03 22:46:42 -0400

    small changes

--- app/src/main/java/com/tec9rushbgroup/ourspace/CreateSpace.java
+++ app/src/main/java/com/tec9rushbgroup/ourspace/CreateSpace.java
@@ -1,49 +1,26 @@
 package com.tec9rushbgroup.ourspace;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.TextUtils;
-import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.net.URI;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class CreateSpace extends AppCompatActivity {
     private TextView welcomeTV;
@@ -57,15 +34,15 @@ public class CreateSpace extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_space);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         databaseSpaces = FirebaseDatabase.getInstance().getReference("Spaces");
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
-        welcomeTV = findViewById(R.id.welcome_text);
+        welcomeTV = findViewById(R.id.create_space_text);
         getEmail = findViewById(R.id.get_email);
         backButton = findViewById(R.id.back_button);
         inviteButton = findViewById(R.id.invite_button);
