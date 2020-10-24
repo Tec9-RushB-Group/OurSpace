fdeabcfc639b1013c6585398ce9d922c236c656e
levi6y <yangliu7@cmail.carleton.ca>
2020-10-24 14:57:15 +0800

    Update AddAnniversary.java

--- app/src/main/java/com/tec9rushbgroup/ourspace/AddAnniversary.java
+++ app/src/main/java/com/tec9rushbgroup/ourspace/AddAnniversary.java
@@ -3,18 +3,23 @@ package com.tec9rushbgroup.ourspace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AddAnniversary extends AppCompatActivity {
     private Button addAnniversary, backToAnniversaries;
-
+    private TextView welcomeTV, sloganTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_anniversary);
-
+        welcomeTV = findViewById(R.id.welcome_text);
+        sloganTV = findViewById(R.id.slogan_text);
+        welcomeTV.setTypeface(Typeface.createFromAsset(getAssets(), "logo.ttf"));
+        sloganTV.setTypeface(Typeface.createFromAsset(getAssets(), "slogan.ttf"));
 
         //Buttons
         addAnniversary = findViewById(R.id.add_anniversary_button);
