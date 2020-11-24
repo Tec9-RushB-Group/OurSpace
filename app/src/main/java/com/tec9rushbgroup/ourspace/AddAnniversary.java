f950dc38106655b0886fdd6eec03b0b12ca39396
Austin Wright <austinwright000@gmail.com>
2020-11-24 17:47:57 -0500

    Added ability to pick a date from a calendar. Still have to save that date and display it when you get to the anniversary page

--- app/src/main/java/com/tec9rushbgroup/ourspace/AddAnniversary.java
+++ app/src/main/java/com/tec9rushbgroup/ourspace/AddAnniversary.java
@@ -1,21 +1,43 @@
 package com.tec9rushbgroup.ourspace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
-public class AddAnniversary extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import cn.iwgang.countdownview.CountdownView;
+
+public class AddAnniversary extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private Button addAnniversary, backToAnniversaries;
     private TextView welcomeTV, sloganTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_anniversary);
+
+        Button btnDatePicker = findViewById(R.id.btnDatePicker);
+        btnDatePicker.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"date picker");
+            }
+        });
+
         welcomeTV = findViewById(R.id.welcome_text);
         sloganTV = findViewById(R.id.slogan_text);
         welcomeTV.setTypeface(Typeface.createFromAsset(getAssets(), "logo.ttf"));
@@ -42,6 +64,70 @@ public class AddAnniversary extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+        String pickerDateString = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+//        TextView tvDatePicker = findViewById(R.id.textViewContent);
+//        CountdownView myCountdownView = findViewById(R.id.countdownView);
+//
+//        try{
+//            tvDatePicker.setText(pickerDateString);
+//            Date now = new Date();
+//
+//            long currentDate = now.getTime();
+//            long pickerDate = calendar.getTimeInMillis();
+//            long countDownToPickerDate = pickerDate - currentDate;
+//            myCountdownView.start(countDownToPickerDate);
+//        } catch(Exception e){
+//            e.printStackTrace();
+//        }
+    }
+    //    public static CharSequence getCountdownText(Context context, Date futureDate){
+//        StringBuilder countdownText = new StringBuilder();
+//
+//        long timeRemaining = futureDate.getTime() - new Date().getTime();
+//
+//        if(timeRemaining > 0){
+//            Resources resources = context.getResources();
+//
+//            //Calculate the days, hours, minutes and seconds until anniversary
+//            //We also have to make sure to subtract from the total value or else we won't get proper values
+//            int days = (int) TimeUnit.MILLISECONDS.toDays(timeRemaining);
+//            timeRemaining -= TimeUnit.MILLISECONDS.toDays(timeRemaining);
+//            int hours = (int) TimeUnit.MILLISECONDS.toHours(timeRemaining);
+//            timeRemaining -= TimeUnit.MILLISECONDS.toHours(timeRemaining);
+//            int minutes = (int) TimeUnit.MILLISECONDS.toMinutes(timeRemaining);
+//            timeRemaining -= TimeUnit.MILLISECONDS.toMinutes(timeRemaining);
+//            int seconds = (int)TimeUnit.MILLISECONDS.toSeconds(timeRemaining);
+//
+//
+//            //now add these quantities to a string
+//            if(days > 0){
+//                countdownText.append(resources.getQuantityString(R.plurals.days,days,days));
+//                countdownText.append(" ");
+//            }
+//            if(hours > 0){
+//                countdownText.append(resources.getQuantityString(R.plurals.hours,hours,hours));
+//                countdownText.append(" ");
+//            }
+//            if(minutes > 0){
+//                countdownText.append(resources.getQuantityString(R.plurals.minutes,minutes,minutes));
+//                countdownText.append(" ");
+//            }
+//            if(seconds > 0){
+//                countdownText.append(resources.getQuantityString(R.plurals.seconds,seconds,seconds));
+//                countdownText.append(" ");
+//            }
+//        }
+//        return countdownText.toString();
+//    }
+
     @Override
     public void onBackPressed() {
         String uid = getIntent().getStringExtra("uid");
